7e8e592ffe2c807e9370e44fdbebad792acaab08
rakshit suneja <rakshits2105@gmail.com>
2026-01-27 15:34:58 +0530

    Changes in frontend

---
+++ frontend/tripmind/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind is finally alive 🔥
+      </h1>
+    </div>
+  )
+}
